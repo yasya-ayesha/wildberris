ea748155d4881e5ab28dbb1ddb2b026270cb27f2
Yasya Smielova <blackcat90inc@gmail.com>
2021-11-21 13:07:04 +0200

    add search, rendering filtered goods on links

--- src/modules/getGoods.js
+++ src/modules/getGoods.js
@@ -1,18 +1,67 @@
 const getGoods = () => {
   const links = document.querySelectorAll('.navigation-link');
+  
+  if (window.location.pathname === '/index.html') {
+    const allBtn = document.querySelector('.more');
+    allBtn.addEventListener('click', (event) => {
+      event.preventDefault();
+      getData();
+    })
+  }
+  
+  const renderGoods = goods => {
+    const goodsContainer = document.querySelector('.long-goods-list');
+    goodsContainer.innerHTML = '';
 
-  const getData = () => {
+    goods.forEach(({label, img, name, price, id, description}) => {
+      const goodBlock = document.createElement('div');
+
+      goodBlock.classList.add('col-lg-3');
+      goodBlock.classList.add('col-sm-6');
+
+      goodBlock.innerHTML = `
+        <div class="goods-card">
+					<span class="label ${label ? null : 'd-none'}">${label}</span>
+					<img src="db/${img}" alt="${name}" class="goods-image">
+					<h3 class="goods-title">${name}</h3>
+					<p class="goods-description">${description}</p>
+					<button class="button goods-card-btn add-to-cart" data-id="${id}">
+						<span class="button-price">$${price}</span>
+					</button>
+				</div>
+      `
+
+      goodsContainer.append(goodBlock)
+    })
+  }
+
+  const getData = (value, category) => {
     fetch('https://testproject2-672ba-default-rtdb.firebaseio.com/db.json')
       .then(res => res.json())
-      .then(data => localStorage.setItem('goods', JSON.stringify(data)));
+      .then(data => {
+        const array = category ? data.filter((item) => item[category] === value) : data;
+        localStorage.setItem('goods', JSON.stringify(array));
+
+        if (window.location.pathname !== '/goods.html') {
+          window.location.href = '/goods.html';
+        } else {
+          renderGoods(array);
+        }
+      })
   };
 
   links.forEach((link) => {
     link.addEventListener('click', (event) => {
       event.preventDefault();
-      getData();
+      const linkValue = link.textContent;
+      const category = link.dataset.field;
+      getData(linkValue, category);
     })
   });
+
+  if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
+    renderGoods(JSON.parse(localStorage.getItem('goods')));
+  }
 };
 
 export default getGoods;
--- src/modules/search.js
+++ src/modules/search.js
@@ -2,9 +2,51 @@ const search = function () {
   const input = document.querySelector('.search-block > input');
   const searchBtn = document.querySelector('.search-block > button');
 
+    const renderGoods = goods => {
+    const goodsContainer = document.querySelector('.long-goods-list');
+    goodsContainer.innerHTML = '';
+
+    goods.forEach(({label, img, name, price, id, description}) => {
+      const goodBlock = document.createElement('div');
+
+      goodBlock.classList.add('col-lg-3');
+      goodBlock.classList.add('col-sm-6');
+
+      goodBlock.innerHTML = `
+        <div class="goods-card">
+					<span class="label ${label ? null : 'd-none'}">${label}</span>
+					<img src="db/${img}" alt="${name}" class="goods-image">
+					<h3 class="goods-title">${name}</h3>
+					<p class="goods-description">${description}</p>
+					<button class="button goods-card-btn add-to-cart" data-id="${id}">
+						<span class="button-price">$${price}</span>
+					</button>
+				</div>
+      `
+
+      goodsContainer.append(goodBlock)
+    })
+  }
+
+  const getData = (value) => {
+    fetch('https://testproject2-672ba-default-rtdb.firebaseio.com/db.json')
+      .then(res => res.json())
+      .then(data => {
+        const array = data.filter(good => good.name.toLowerCase().includes(value.toLowerCase()));
+        localStorage.setItem('goods', JSON.stringify(array));
+
+        if (window.location.pathname !== '/goods.html') {
+          window.location.href = '/goods.html';
+        } else {
+          renderGoods(array);
+        }
+      })
+  };
+
   searchBtn.addEventListener('click', () => {
-    console.log(input.value); 
+    getData(input.value);
   })
+
 }
 
 export default search;
